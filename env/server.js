172399911d402be8049018a841de7a661c68d985
ahs59 <andrew@shidel.com>
2014-06-09 19:28:14 -0400

    Made some improvments to the make file.

--- env/server.js
+++ env/server.js
@@ -73,7 +73,7 @@ function pause(){
 	module.stack = stack;
 	module.sibCount = sibCount;
 	fs.writeFileSync("pause.json", JSON.stringify(module));
-	process.exit(1);
+	process.exit(0);
 }
 
 //Main function where things start.
