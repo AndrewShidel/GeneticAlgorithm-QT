e6a1e431aa5247d1428c5c2deefb1f5cd93a4c6b
shidel-dev <andrewshidel@gmail.com>
2014-09-20 18:18:25 -0400

    Maker now utilizes the Library.h methods.

--- Algorithm/Maker.cpp
+++ Algorithm/Maker.cpp
@@ -17,29 +17,134 @@
 
 using namespace std;
 
-Maker::Maker(std::string baseDir, std::string child, std::string parent, int mutationRate, int insertRate){
-    path = baseDir;
-    gen = child;
-    oldGen = parent;
-    mutRate = mutationRate;
-    InsertRate = insertRate;
+Maker::Maker(){
+    
 }
 
-//Takes a path to a file (minus the filename), makes small changes, and creates a new file with the name "gen".
-void Maker::make(string outName, string inName){
+typedef struct org_struct{
+    std::map<int, int> *offsetMap;
+    std::streambuf * inputStream;
+} org;
 
+//TOTO: Try and call this from the main code.
+int Maker::make2(std::string *baseDir,  std::pair<std::string, std::map<int, int> > *child, std::pair<std::string, std::map<int, int> > *parent1, std::pair<std::string, std::map<int, int> > *parent2, int baseGeneLength, int numGenes, int mutationRate, int insertRate, Language *lan){
+    
     //Seed the random number generator.
     unsigned int time_ui = static_cast<unsigned int>( time(NULL) );
     srand( time_ui );
+    
+    //Get the path for the new file.
+    string dirPath=*baseDir+child->first;
+    
+    //Child's output stream
+    ofstream out(dirPath.c_str(), ios::out | ios::binary);
+    
+    //Create input streams for each parent.
+    std::ifstream istr1 ((*baseDir + parent1->first).c_str());
+    std::ifstream istr2 ((*baseDir + parent2->first).c_str());
+    
+    //Create parent 1's org object.
+    org *parent_1 = (org *)malloc(sizeof(org));
+    parent_1->offsetMap=&(parent1->second);
+    parent_1->inputStream = istr1.rdbuf();
+    
+    //Create parent 2's org object.
+    org *parent_2 = (org *)malloc(sizeof(org));
+    parent_2->offsetMap=&(parent2->second);
+    parent_2->inputStream = istr2.rdbuf();
+    
+    //Pointer to whichever org is currently in use.
+    org *inUse = (org *)malloc(sizeof(org));
+    org *notInUse = (org *)malloc(sizeof(org));
+    
+    //The genes covered so far, and the bits covered so far within a gene.
+    int genesCovered=0, bitsCovered;
+    
+    //Marker offsets for both parents.
+    int inUseOffset = 0;
+    int notInUseOffset = 0;
+    
+    //An iterator for searching through each parent's offset Map.
+    std::map<int,int>::iterator it;
+    char ch;
+    int nextc=0;
+    
+    if (istr1 && istr2){
+        while (genesCovered<numGenes+1) {
+            if (rand()%2==0){
+                inUse = parent_1;
+                notInUse = parent_2;
+            }else{
+                inUse = parent_2;
+                notInUse = parent_1;
+            }
+            //Get the marker offset for the first parent.
+            it = inUse->offsetMap->find(genesCovered);
+            if (it!=inUse->offsetMap->end()){
+                inUseOffset = (*it).second;
+                (child->second)[genesCovered] = inUseOffset;
+            }else{
+                inUseOffset = 0;
+            }
+            //Get the maqker offset for the second parent.
+            it = notInUse->offsetMap->find(genesCovered);
+            if (it!=notInUse->offsetMap->end()){
+                notInUseOffset =(*it).second;
+            }else{
+                notInUseOffset = 0;
+            }
+            
+            bitsCovered = 0;
+            //Get each byte of the gene.
+            while (bitsCovered < baseGeneLength+inUseOffset && nextc!=EOF){
+                //Get the next charactor.
+                ch = inUse->inputStream->sgetc();
 
-    //Get the path for the output file.
-    std::ostringstream oss;
-    oss << path<<gen<<"/"<<outName;
-    string dirPath=oss.str();
+                //Make an intentional mistake.
+                if (1+rand()%mutationRate==1){ ch=lan->randVal();}
+                if (1+rand()%insertRate==1){
+                    out << lan->randVal();
+                    //Increment the offset.
+                    ++((*(inUse->offsetMap))[genesCovered]);
+                }
+                
+                //Write the char to the child's file.
+                out << ch;
 
+                ++bitsCovered;
+                nextc = inUse->inputStream->snextc();
+                
+            }
+            //Increment the unused inputStream to the next gene.
+            notInUse->inputStream->pubseekoff( baseGeneLength+notInUseOffset, ios_base::cur);
+            ++genesCovered;
+        }
+        
+    }else{
+        return 1;
+    }
+    //Give the child execute permissions.
+    chmod(dirPath.c_str(), S_IRWXU);
+    
+    istr1.close();
+    istr2.close();
+    out.close();
+    return 0;
+}
+
+//Takes a path to a file (minus the filename), makes small changes, and creates a new file with the name "gen".
+void Maker::make(std::string *path, std::string *gen, std::string *oldGen, int mutRate, int InsertRate){
+    
+    //Seed the random number generator.
+    unsigned int time_ui = static_cast<unsigned int>( time(NULL) );
+    srand( time_ui );
+    
+    //Get the path for the new file.
+    string dirPath=*path+*gen;
+    
     //Create input and output streams.
-    ofstream out((dirPath).c_str(), ios::out | ios::binary);
-    std::ifstream istr ((path+oldGen+"/"+inName).c_str());
+    ofstream out(dirPath.c_str(), ios::out | ios::binary);
+    std::ifstream istr ((*path+*oldGen).c_str());
     
     //If the input file exists.
     if (istr) {
@@ -52,12 +157,12 @@ void Maker::make(string outName, string inName){
             //Make an intentional mistake.
             if (1+rand()%mutRate==1){ ch=(char)(1+rand()%255);}
             if (1+rand()%InsertRate==1){ out << (char)(1+rand()%255);}
-
+            
             //Write the charactor to the output file.
             out << ch;
-        //Get next charactor, and loop.
+            //Get next charactor, and loop.
         } while ( pbuf->snextc() != EOF );
-
+        
         //Close the streams.
         istr.close();
         out.close();
@@ -66,7 +171,7 @@ void Maker::make(string outName, string inName){
     //Give the new file excecute permissions.
     chmod(dirPath.c_str(), S_IRWXU);
     //exec((char*)( oss2.str() ).c_str());
-    return;    
+    return;
 }
 
 int Maker::split(string path, string filename, string outputDir){
@@ -84,31 +189,31 @@ int Maker::split(string path, string filename, string outputDir){
     for (int i = 0; i < str.length(); i++){
         char c = str[i];
         buffer.push_back(c);
-
-        if (buffer == "#" && bracketCount == 0) 
-            inGlobal = true;
-
+        
+        if (buffer == "#" && bracketCount == 0)
+        inGlobal = true;
+        
         if (c == '{'){
             bracketCount++;
             if (bracketCount == 1){
                 files.push_back("");
                 protoTypes.push_back(buffer.substr(0, buffer.size()-1)+";");
             }
-        } 
+        }
         else if (c == '}') {
             bracketCount--;
             buffer.push_back(str[++i]);
-
+            
             if (!inMain)
-                files[files.size()-1].append(buffer);
+            files[files.size()-1].append(buffer);
             else{
                 files[0].append(buffer);
                 if (bracketCount==0) inMain = false;
             }
             buffer = "";
-            continue;       
+            continue;
         }
-
+        
         //If not in a function.
         if (bracketCount==0){
             if (c=='\n'){
@@ -116,7 +221,7 @@ int Maker::split(string path, string filename, string outputDir){
                     globals.append(buffer);
                     inGlobal = false;
                 }else
-                    files[0].append(buffer);
+                files[0].append(buffer);
                 buffer = "";
             }
         }else if (bracketCount==1){
@@ -134,7 +239,7 @@ int Maker::split(string path, string filename, string outputDir){
             }
         }
     }
-
+    
     for (int i = 0; i < files.size(); i++){
         for(int j = 0; j<protoTypes.size(); j++){
             //std::stringstream sstm;
@@ -143,14 +248,14 @@ int Maker::split(string path, string filename, string outputDir){
             files[i].insert(0,"extern "+protoTypes[j]);
         }
         files[i].insert(0, globals);
-
+        
         string ext;
         if (i==0)
-            ext = ".cpp";
+        ext = ".cpp";
         else
-            ext = ".cpp";
+        ext = ".cpp";
         ofstream file;
-
+        
         std::stringstream sstm;
         sstm << outputDir<<i<<ext;
         file.open (sstm.str());
--- Algorithm/Maker.h
+++ Algorithm/Maker.h
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <string>
+#include <map>
+
+#include "Language.h"
 
 class Maker{
 	public:
-		Maker(std::string baseDir, std::string child, std::string parent, int mutationRate, int insertRate);
-		void make(std::string resultID, std::string inputID);
+		Maker();
+		static void make(std::string *baseDir, std::string *child, std::string *parent, int mutationRate, int insertRate);
+        static int make2(std::string *baseDir,  std::pair<std::string, std::map<int, int> > *child, std::pair<std::string, std::map<int, int> > *parent1, std::pair<std::string, std::map<int, int> > *parent2, int baseGeneLength, int numGenes, int mutationRate, int insertRate, Language *lan);
+
 		static int split(std::string path, std::string filename, std::string outputDir);
-	private:
-		std::string path;
-		std::string gen;
-		std::string oldGen;
-		int mutRate;
-		int InsertRate;
+
 };
